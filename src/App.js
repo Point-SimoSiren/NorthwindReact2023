0ba8e92d5455acbcfb11bb49f4146aae08c5587a
Simo Sirén <simo.siren1@careeria.fi>
2023-09-27 13:07:16 +0300

    1 päivä purkissa

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import './App.css'
 import Laskuri from './Laskuri'
+import React, {useState} from 'react'
+import Posts from './Posts'
 
 const App = () => {
+
+  // App componentin state
+  const [showLaskuri, setShowLaskuri] = useState(false)
 
   return (
     <div className="App">
      
         <h1>Northwind Corporation</h1>
       
-        <Laskuri />
+        {!showLaskuri && <button onClick={() => setShowLaskuri(true)}>näytä laskuri</button>}
+        {showLaskuri && <button onClick={() => setShowLaskuri(false)}>piilota laskuri</button>}
+
+        {showLaskuri && <Laskuri />}
+
+        <Posts />
 
     </div>
   )
