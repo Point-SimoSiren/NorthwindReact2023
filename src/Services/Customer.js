eb9196792c41570a08d8c5b7181c0025712fc1a5
Simo Sirén <simo.siren1@careeria.fi>
2023-11-02 14:37:24 +0200

    token mukaan pyyntöihin

--- src/Services/Customer.js
+++ src/Services/Customer.js
@@ -2,24 +2,44 @@ import axios from 'axios'
 
 const baseUrl = "https://localhost:7128/api/customers"
 
+let token = null;
+
+// Tämä on metodi jota kutsutaan ennen kuin tehdään muu pyyntö serviceen
+// Parametrina annetaan token joka otetaan local storagesta
+const setToken = newToken => {
+    token = `bearer ${newToken}`
+}
+
 const getAll = () => {
-    const request = axios.get(baseUrl)
+    const config = {
+        headers: { Authorization: token },
+    }
+    const request = axios.get(baseUrl, config)
     return request.then(response => response.data)
 }
 
 const addNew = (object) => {
-    const request = axios.post(baseUrl, object)
+    const config = {
+        headers: { Authorization: token },
+    }
+    const request = axios.post(baseUrl, object, config)
     return request.then(response => response.data)
 }
 
 const remove = (id) => {
-    const request = axios.delete(baseUrl + "/" + id)
+    const config = {
+        headers: { Authorization: token },
+    }
+    const request = axios.delete(baseUrl + "/" + id, config)
     return request.then(response => response.data)
 }
 
 const update = (customer) => {
-    const request = axios.put(baseUrl + "/" + customer.customerId, customer)
+    const config = {
+        headers: { Authorization: token },
+    }
+    const request = axios.put(baseUrl + "/" + customer.customerId, customer, config)
     return request.then(response => response.data)
 }
 
-export default {getAll, addNew, remove, update}
+export default {getAll, addNew, remove, update, setToken}
